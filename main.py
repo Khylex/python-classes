be737acba880d3aabf899ecdacebab65c782f3a4
khylex <geoang222@gmail.com>
2022-05-26 18:19:11 +0100

    classes and objects solution

---
+++ main.py
@@ -0,0 +1,20 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+ def _init_(self, name, age, track, score):
+  self.name = ("Angel")
+  self.age = int(27)
+  self.track = ("full-stack")
+  self.score = (99.9)  
+  
+ def sayHi(self):
+    print('Hello, my name is ' + self.name + ', I am ' + self.age + ' years old!' + 'i am in the' + self.track + 'hoping to get a score of '+ self.score)
+
+
+
+# Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# #Expected methods
+# Bob.change_name("Peter")
+# Bob.change_age(34)
+# Bob.add_track("UI/UX")
+# Bob.get_score()
